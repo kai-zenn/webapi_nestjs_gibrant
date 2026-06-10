4abaed1d0caafc06031f7dea4c54796771658845
Kaizen <gibrantsyareza@gmail.com>
2026-06-10 15:45:14 +0700

    adding typeorm transform to automaticaly format any data passing from codebase to database

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 }
 bootstrap();

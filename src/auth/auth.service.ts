f94e26047a62b443632df2a2a120fee3f0313274
Kaizen <gibrantsyareza@gmail.com>
2026-06-12 14:24:35 +0700

    adding login method, it will first search an existing user and compare the hashed password. after it match, it will return a jwt token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,21 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
 import { UserService } from 'src/user/user.service';
+import { LoginResponseDTO, LoginRequestDTO } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async register(dto: CreateUserDto) {
     try {
@@ -19,6 +29,28 @@ export class AuthService {
     } catch (error) {
       throw new InternalServerErrorException(`
         gagal mendaftar karena: ${error}`);
+    }
+  }
+  async login(dto: LoginRequestDTO): Promise<LoginResponseDTO> {
+    try {
+      const { username, password } = dto;
+      const user = await this.userService.findByUsername(username);
+      if (!user) {
+        throw new NotFoundException('Username tidak ditemukan');
+      }
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (!isPasswordMatch) {
+        throw new UnauthorizedException('Password salah');
+      }
+      const payload = {
+        sub: user.id,
+        username: user.username,
+        email: user.email,
+      };
+      const token = await this.jwtService.signAsync(payload);
+      return { token };
+    } catch (error) {
+      throw new InternalServerErrorException(`Gagal login karena: ${error}`);
     }
   }
 }

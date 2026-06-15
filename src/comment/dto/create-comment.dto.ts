27e172fbb94c5369b251b8a1b01dc38cd57095f0
Kaizen <gibrantsyareza@gmail.com>
2026-06-15 19:43:58 +0700

    done configuring DTO and entity for comment and adding ManyToOne relation with user and post

---
+++ src/comment/dto/create-comment.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCommentDTO {
+  @IsNotEmpty({ message: 'Komentar tidak boleh kosong' })
+  @IsString()
+  body: string;
+
+  // @IsUUID()
+  // userId: string;
+
+  // @IsNumber()
+  // postId: number;
+}
---
+++ src/comment/entities/comment.entity.ts
@@ -0,0 +1,30 @@
+import { PostEntity } from 'src/post/entities/post.entity';
+import { UserEntity } from 'src/user/entities/user.entity';
+import {
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  Column,
+  Entity,
+  ManyToOne,
+} from 'typeorm';
+import { JoinColumn } from 'typeorm/browser';
+
+@Entity('comments')
+export class CommentEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  body: string;
+
+  @ManyToOne(() => UserEntity, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user: UserEntity;
+
+  @ManyToOne(() => PostEntity, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'post_id' })
+  post: PostEntity;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
